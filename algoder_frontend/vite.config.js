4ef254c600efc82a483e58a8f6b56d6e3245181d
harish suthar <harishsuthar7023@gmail.com>
2025-06-02 16:54:01 +0530

    change vite config

--- algoder_frontend/vite.config.js
+++ algoder_frontend/vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(),tailwindcss()],
+  plugins: [react(), tailwindcss()],
+  base: '/', // ✅ React routing को ठीक रखने के लिए ज़रूरी है
 })
