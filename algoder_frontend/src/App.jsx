a5ab3b73f61ba1c7da3596635425ad013496a472
harish suthar <harishsuthar7023@gmail.com>
2025-07-28 09:42:05 +0530

    fix some bugs

--- algoder_frontend/src/App.jsx
+++ algoder_frontend/src/App.jsx
@@ -1,51 +1,55 @@
+import { Suspense, lazy } from 'react';
 import { Routes, Route } from 'react-router-dom';
-import Home from './pages/HomePage';
-import Register from './pages/Register';
-import Login from './pages/LoginPage';
-import ProductPage from './pages/ProductPage';
-import ProductDetail from './pages/ProducDetail';
-import CheckoutPage from './pages/CheckoutPage';
-import MyOrders from './pages/MyOrders';
-import OrderCheck from './pages/OrderCheck';
-import ProductAdmin from './pages/ProductAdmin';
-import Dashboard from './pages/Dashboard';
-import AdminProducts from './pages/AdminProducts';
-import About from './pages/About';
-import Mycourse from './pages/Mycourse';
-import CourseAdmin from './pages/CourseAdmin';
-import Courses from './pages/Courses';
-// import EditProductForm from './pages/EditProductForm';
+
+// ✅ Lazy-loaded pages
+const Home = lazy(() => import('./pages/HomePage'));
+const Register = lazy(() => import('./pages/Register'));
+const Login = lazy(() => import('./pages/LoginPage'));
+const ProductPage = lazy(() => import('./pages/ProductPage'));
+const ProductDetail = lazy(() => import('./pages/ProducDetail'));
+const CheckoutPage = lazy(() => import('./pages/CheckoutPage'));
+const MyOrders = lazy(() => import('./pages/MyOrders'));
+const OrderCheck = lazy(() => import('./pages/OrderCheck'));
+const ProductAdmin = lazy(() => import('./pages/ProductAdmin'));
+const Dashboard = lazy(() => import('./pages/Dashboard'));
+const AdminProducts = lazy(() => import('./pages/AdminProducts'));
+const About = lazy(() => import('./pages/About'));
+const Mycourse = lazy(() => import('./pages/Mycourse'));
+const CourseAdmin = lazy(() => import('./pages/CourseAdmin'));
+const Courses = lazy(() => import('./pages/Courses'));
+// const EditProductForm = lazy(() => import('./pages/EditProductForm')); // Uncomment if needed
 
 function App() {
   return (
-    <Routes>
-      {/* Public Routes */}
-      <Route path="/" element={<Home />} />
-      <Route path="/register" element={<Register />} />
-      <Route path="/login" element={<Login />} />
+    <Suspense fallback={<div className="text-center text-white mt-20">Loading...</div>}>
+      <Routes>
+        {/* Public Routes */}
+        <Route path="/" element={<Home />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/login" element={<Login />} />
 
-      {/* Product Routes */}
-      <Route path="/products" element={<ProductPage />} />
-      <Route path="/product/:id" element={<ProductDetail />} />
-      <Route path="/checkout/:id/:types" element={<CheckoutPage />} />
-      <Route path="/about" element={<About />} />
+        {/* Product Routes */}
+        <Route path="/products" element={<ProductPage />} />
+        <Route path="/product/:id" element={<ProductDetail />} />
+        <Route path="/checkout/:id/:types" element={<CheckoutPage />} />
+        <Route path="/about" element={<About />} />
 
-      {/* User Orders */}
-      <Route path="/myorders" element={<MyOrders />} />
-      <Route path="/ordercheck" element={<OrderCheck />} />
+        {/* User Orders */}
+        <Route path="/myorders" element={<MyOrders />} />
+        <Route path="/ordercheck" element={<OrderCheck />} />
 
-      {/* Admin Routes */}
-      <Route path="/productadmin" element={<ProductAdmin />} />
-      <Route path="/dashboard" element={<Dashboard />} />
-      <Route path="/adminproducts" element={<AdminProducts />} />
-      <Route path="/mycourse" element={<Mycourse />} />
+        {/* Admin Routes */}
+        <Route path="/productadmin" element={<ProductAdmin />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/adminproducts" element={<AdminProducts />} />
+        <Route path="/mycourse" element={<Mycourse />} />
+        <Route path="/courseadmin" element={<CourseAdmin />} />
+        <Route path="/courses" element={<Courses />} />
 
-      <Route path="/courseadmin" element={<CourseAdmin />} />
-      <Route path="/courses" element={<Courses />} />
-
-
-      {/* <Route path="/product/:id/edit" element={<EditProductForm />} /> */}
-    </Routes>
+        {/* Optional Route */}
+        {/* <Route path="/product/:id/edit" element={<EditProductForm />} /> */}
+      </Routes>
+    </Suspense>
   );
 }
 
